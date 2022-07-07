0634edb108b6feaaf8888ce3b2e63ce24576554f
Jian Kim <jian7061@gmail.com>
2022-07-07 16:42:58 +0900

    add hook - useTokenContract

---
+++ src/hooks/useTokenContract.ts
@@ -0,0 +1,12 @@
+import { useContract } from '@starknet-react/core';
+import { Abi } from 'starknet';
+
+import { EXAMPLE_ABI } from '../ABIs';
+import { EXAMPLE_ADDRESS } from '../constants';
+
+export function useTokenContract() {
+  return useContract({
+    abi: EXAMPLE_ABI as Abi,
+    address: EXAMPLE_ADDRESS,
+  });
+}
