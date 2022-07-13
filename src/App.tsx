a41859efd0ba735ea34d8e173f3f49d6a6287ba1
Jian Kim <jian7061@gmail.com>
2022-07-13 21:38:13 +0900

    restructure app removing chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Home } from './pages';
-import { Navbar } from './components';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { getInstalledInjectedConnectors, StarknetProvider } from '@starknet-react/core';
-import { extendTheme, ChakraProvider, Container } from '@chakra-ui/react';
 import { Provider } from 'starknet';
-
-const theme = extendTheme();
+import { GlobalStyle } from './theme';
 
 function App() {
   const connectors = getInstalledInjectedConnectors();
 
   return (
-    <ChakraProvider theme={theme}>
+    <>
+      <GlobalStyle />
       <StarknetProvider
         connectors={connectors}
         autoConnect
         defaultProvider={new Provider({ baseUrl: 'http://localhost:500' })}>
         <Router>
-          <Navbar />
-          <Container maxW='2xl' bg='blue.200' centerContent>
-            <Routes>
-              <Route path='/' element={<Home />} />
-            </Routes>
-          </Container>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
         </Router>
       </StarknetProvider>
-    </ChakraProvider>
+    </>
   );
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export * from './ConnectWallet';
-export * from './Navbar';
 export * from './TransactionList';
