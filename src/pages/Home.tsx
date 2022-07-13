8592a0bcd689e830ab0365adda5982b4c0010a7d
Jian Kim <jian7061@gmail.com>
2022-07-13 13:40:25 +0900

    useConnectors to connect wallets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,28 @@ import React, { useMemo, useState } from 'react';
 import { useStarknetCall } from '@starknet-react/core';
 import { toBN } from 'starknet/dist/utils/number';
 import { Button } from '@chakra-ui/react';
-import { useStarknet } from '@starknet-react/core';
+import { useStarknet, useConnectors } from '@starknet-react/core';
 
 export const Home = () => {
   const [watch, setWatch] = useState(true);
   const { account } = useStarknet();
+  const { available, connect, disconnect } = useConnectors();
+  if (account) {
+    return (
+      <div>
+        <p>Account: {account}</p>
+        <button onClick={() => disconnect()}>Disconnect</button>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Button
-        colorScheme='teal'
-        _hover={{
-          background: 'white',
-          color: 'teal.500',
-        }}>
-        Hover me
-      </Button>
-      <div>{account}</div>
+      {available.map((connector) => (
+        <button key={connector.id()} onClick={() => connect(connector)}>
+          {`Connect ${connector.name()}`}
+        </button>
+      ))}
     </div>
   );
 };
